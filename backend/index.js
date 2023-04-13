f786f9b81ca010416399643ec6a24cb4e50a9897
iamshiv007 <iamshiv20032003@gmail.com>
2023-04-13 11:02:22 +0530

    login signup completed

---
+++ backend/index.js
@@ -0,0 +1,65 @@
+const express = require('express')
+const dotenv = require('dotenv').config()
+const app = express()
+const bodyParser = require("body-parser")
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const fileUpload = require("express-fileupload")
+const path = require("path")
+
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(cors({
+    origin: [process.env.ORIGIN],
+    credentials: true
+}))
+app.use(fileUpload())
+
+// Handling Uncaught Exception
+process.on("uncaughtException", (err) => {
+    console.log(`Error: ${err.message}`)
+    console.log('Shutting down the server due to Uncaught Exception')
+    process.exit(1)
+})
+
+/** Database connection */
+const connect = require('./Database')
+connect()
+
+/** Routes import */
+const router = require('./routes/Router')
+app.use('/api/v1', router)
+
+// Middleware for error handling
+const errroMiddleware = require('./middleware/Error')
+app.use(errroMiddleware)
+
+const port = process.env.PORT || 7000
+
+const server = app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`)
+})
+
+// Unhandled Promise Rejection
+process.on("unhandledRejection", (err) => {
+    console.log(`Error: ${err.message}`)
+    console.log('Shutting down the server due to Unhandled Promise Rejection')
+
+    server.close(() => {
+        process.exit(1)
+    })
+})
+
+// Statis files
+app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+app.get('*', function(req, res){
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+})
+
+// text server
+app.get('/', (req, res) => {
+    res.send("Server Tested")
+})
+
