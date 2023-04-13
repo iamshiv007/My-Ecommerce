c249028dcc9b065279ddcf7b516f244de784667b
iamshiv007 <iamshiv20032003@gmail.com>
2023-04-13 18:14:29 +0530

    romove console.log

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,6 @@ app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(cors())
-console.log(process.env.ORIGIN)
 app.use(cors({
     origin: [process.env.ORIGIN],
     credentials: true
